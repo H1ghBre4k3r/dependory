9d98f28b55f9e763ce0df6ce04fff5778e588cac
Louis Meyer <h1ghbre4k3r@dev.bre4k3r.de>
2020-06-01 14:37:00 +0200

    Add some comments

--- src/injectable.ts
+++ src/injectable.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata";
 
+/**
+ * Make a class injectable.
+ */
 export function Injectable(): (clazz: any) => void {
     return function(clazz: any): void {
         if (!clazz) {
@@ -30,21 +33,34 @@ export function Injectable(): (clazz: any) => void {
 class Registry {
     private static registry = new Map<string, any>();
 
-    public static isClass(clazz: any): boolean {
+    /**
+     * Check, if a class is actually an ES6 class.
+     * @param clazz object, for which to check, if it is an ES6 class
+     */
+    public static isClass(clazz: { toString(): string }): boolean {
         return /^\s*class/.test(clazz.toString());
     }
 
-    public static getHash(clazz: any): string {
-        const str = clazz.toString();
+    /**
+     * Generate the hash for anything, which has a "toString" method.
+     * @param object object to generate the hash for
+     */
+    public static getHash(object: { toString(): string }): string {
+        const str = object.toString();
         let hash = 0;
         for (let i = 0; i < str.length; i++) {
             const chr = str.charCodeAt(i);
             hash = (hash << 5) - hash + chr;
-            hash |= 0; // Convert to 32bit integer
+            hash |= 0;
         }
         return hash.toString();
     }
 
+    /**
+     * Add a value to the registry.
+     * @param hash hash of the value to add
+     * @param instance instance to add to the registry
+     */
     public static set(hash: string, instance: any): void {
         if (Registry.registry.has(hash)) {
             throw new Error(`"${hash}" already in registry!`);
@@ -53,6 +69,10 @@ class Registry {
         Registry.registry.set(hash, instance);
     }
 
+    /**
+     * Get the value which is stored for a hash.
+     * @param hash hash to get the stored value for
+     */
     public static get(hash: string): any {
         return Registry.registry.get(hash);
     }
