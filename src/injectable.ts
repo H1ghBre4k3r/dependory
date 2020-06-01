0706f0b4e909ff0173511c8981a1a1c19b3026c5
Louis Meyer <h1ghbre4k3r@dev.bre4k3r.de>
2020-06-01 16:23:09 +0200

    Add InjectableOptions to provide a way to manage the Registry yourself

--- src/injectable.ts
+++ src/injectable.ts
@@ -1,38 +1,19 @@
 import "reflect-metadata";
 
 /**
- * Make a class injectable.
+ * Options for an injection.
  */
-export function Injectable(): (clazz: any) => void {
-    return function(clazz: any): void {
-        if (!clazz) {
-            throw new Error(`Argument must be of type class`);
-        }
-        // Get constructor arguments
-        const args = Reflect.getMetadata("design:paramtypes", clazz);
-
-        // Fetch all new arguments out of the registry
-        const newArgs = args.map((arg: any) => {
-            const hash = Registry.getHash(arg);
-            let param;
-            // Wait, until all depencies are resolved
-            while (!param) {
-                param = Registry.get(hash);
-            }
-            return param;
-        });
-
-        // Instantiate object and register it in the registry
-        const hash = Registry.getHash(clazz);
-        const instance = new clazz(...newArgs);
-        Registry.set(hash, instance);
-    };
+interface InjectableOptions {
+    /**
+     * Registry for injecting and storing classes. Of none is provided, the default one will be used.
+     */
+    registry?: Registry;
 }
 
-// tslint:disable-next-line: no-unnecessary-class
+/**
+ * Registry for managing the injections.
+ */
 export class Registry {
-    private static registry = new Map<string, any>();
-
     /**
      * Check, if a class is actually an ES6 class.
      * @param clazz object, for which to check, if it is an ES6 class
@@ -57,23 +38,65 @@ export class Registry {
     }
 
     /**
+     * Map for storing singletons.
+     */
+    private registry = new Map<string, any>();
+
+    /**
      * Add a value to the registry.
      * @param hash hash of the value to add
      * @param instance instance to add to the registry
      */
-    public static set(hash: string, instance: any): void {
-        if (Registry.registry.has(hash)) {
+    public set(hash: string, instance: any): void {
+        if (this.registry.has(hash)) {
             throw new Error(`"${hash}" already in registry!`);
         }
 
-        Registry.registry.set(hash, instance);
+        this.registry.set(hash, instance);
     }
 
     /**
      * Get the value which is stored for a hash.
      * @param hash hash to get the stored value for
      */
-    public static get(hash: string): any {
-        return Registry.registry.get(hash);
+    public get(hash: string): any {
+        return this.registry.get(hash);
     }
+}
+
+/**
+ * Default registry to use, if no specific registry is provided.
+ */
+const defaultRegistry = new Registry();
+
+/**
+ * Make a class injectable.
+ * @param options options for the injection
+ */
+export function Injectable(options?: InjectableOptions): (clazz: any) => void {
+    const registry = options?.registry ?? defaultRegistry;
+
+    return function(clazz: any): void {
+        if (!clazz) {
+            throw new Error(`Argument must be of type class`);
+        }
+        // Get constructor arguments
+        const args = Reflect.getMetadata("design:paramtypes", clazz);
+
+        // Fetch all new arguments out of the registry
+        const newArgs = args.map((arg: any) => {
+            const hash = Registry.getHash(arg);
+            let param;
+            // Wait, until all depencies are resolved
+            while (!param) {
+                param = registry.get(hash);
+            }
+            return param;
+        });
+
+        // Instantiate object and register it in the registry
+        const hash = Registry.getHash(clazz);
+        const instance = new clazz(...newArgs);
+        registry.set(hash, instance);
+    };
 }
